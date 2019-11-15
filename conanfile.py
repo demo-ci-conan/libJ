580d0c1a176a2322c526cb01eb6aec22fecd2ea5
javierg <javierg@jfrog.com>
2019-11-15 13:34:09 +0100

    initial

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile, CMake
+
+class LibC(ConanFile):
+    name = "libC"
+    version = "0.0"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+
+    generators = "cmake"
+
+    scm = {"type": "git",
+           "url": "auto",
+           "revision": "auto"}
+
+    def requirements(self):
+        self.requires("libA/0.0@demo/testing")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.install()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses")
+
+    def package_info(self):
+        self.cpp_info.libs = ["libA",]
